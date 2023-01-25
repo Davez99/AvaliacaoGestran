c5af5650ef51f6785d244cc1c0e9379dac22b3fc
Davez99 <davi.mdr@gmail.com>
2023-01-25 15:53:06 -0300

    Adicionando a entidade Fornecedor

---
+++ GestranAPI/Entities/Fornecedor.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestranAPI.Entities
+{
+    public class Fornecedor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int CNPJ { get; set; }
+        public int Telefone { get; set; }
+        public string Email { get; set; }
+    }
+}
